35315c4a1195991798b988216d78dd81b877d2a9
Diego Murillo <diegoa31052004@gmail.com>
2024-06-05 13:57:19 -0500

    Create Hola mundo.py

---
+++ Hola mundo.py
@@ -0,0 +1,10 @@
+print ("Hola mundo")
+print ("Hola","Mundo", "Python", 3,end="XXX")
+print ("Hola","Mundo", "Python", 3,sep="?")
+n="valor"
+print ("Valor es de tipo" + type,(n))
+y="valor"
+print ("tipo de dato de y" + type,(y))
+z = x/y
+print ("tipo de dato de z" + type,(z))
+print (z)
